aa6bddc2718bab1fc115b91de892b0d6ac5b96ed
medicalexcom <medicalex.com@gmail.com>
2025-11-15 14:50:56 -0600

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,13 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware({
-  publicRoutes: ["/", "/sign-in", "/sign-up"],
+  publicRoutes: [
+    "/", 
+    "/sign-in",
+    "/sign-up"
+  ],
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!_next|.*\\..*).*)"],
 };
