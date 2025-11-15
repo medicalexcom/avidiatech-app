ebf1cad754e6ba5ca2bfebe5b5f40e2c7fd14879
medicalexcom <medicalex.com@gmail.com>
2025-11-15 15:53:16 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import type { ReactNode } from "react";
 
 export const metadata = {
   title: "AvidiaTech App",
   description: "Product Data Automation SaaS Platform",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <ClerkProvider>
       <html lang="en">
