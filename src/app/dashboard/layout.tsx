7adcec782569b4a5344f001e342ae69ff462709b
medicalexcom <medicalex.com@gmail.com>
2025-11-15 16:53:55 -0800

    Add dashboard layout with Sidebar and TopNav

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+import type { ReactNode } from 'react';
+import Sidebar from '../../components/Sidebar';
+import TopNav from '../../components/TopNav';
+
+export default function DashboardLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <TopNav />
+      <div className="flex flex-1">
+        <Sidebar />
+        <main className="flex-1 p-6 bg-gray-50">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
