260ee5fcf48283c5bd94be55657e6ecb49ad8f72
medicalexcom <medicalex.com@gmail.com>
2025-11-15 21:00:21 -0800

    Fix dashboard layout to use Sidebar component and correct markup

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,17 +1,18 @@
-import type { ReactNode } from 'react'
-import Sidebar from '../../components/Sidebar'
-import TopNav from '../../components/TopNav'
+import type { ReactNode } from 'react';
+import Sidebar from '../../components/Sidebar';
+import TopNav from '../../components/TopNav';
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col">
       <TopNav />
       <div className="flex flex-1">
-  <<Sidebar /> />
+        <Sidebar />
         <main className="flex-1 p-6 bg-gray-50">
           {children}
         </main>
       </div>
     </div>
-  )
+  );
+}
 }
