4a979446603dd8c94d275c157a9c2c04a15613e4
medicalexcom <medicalex.com@gmail.com>
2025-11-20 23:55:58 -0600

    Delete src directory

--- src/app/providers.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { ClerkProvider } from '@clerk/nextjs';
-import type { ReactNode } from 'react';
-
-export default function Providers({ children }: { children: ReactNode }) {
-  return <ClerkProvider>{children}</ClerkProvider>;
-}
