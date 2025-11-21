2b1c8b9a9638338e8ff807647ce8ab73abced12c
medicalexcom <medicalex.com@gmail.com>
2025-11-21 11:04:24 -0600

    Fix export statement for dynamic in error.tsx

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,7 @@
-export const dynamic = 'force-dynamic';
-
 'use client';
 import { useEffect } from 'react';
+
+export const dynamic = 'force-dynamic';
 
 export default function GlobalError({ error }: { error: Error }) {
   useEffect(() => {
