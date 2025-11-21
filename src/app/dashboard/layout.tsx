ec2a039100421d141b4bed33def9a079b612e545
medicalexcom <medicalex.com@gmail.com>
2025-11-21 11:31:09 -0800

    Restore flex layout and constrain sidebar width
    Restore flex layout and constrain sidebar width in dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -6,10 +6,16 @@ import Sidebar from '@/components/Sidebar';
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <TopNav />
-      <Sidebar />
-      {children}
-    </>
+      <div className="flex flex-1">
+        <aside className="w-64">
+          <Sidebar />
+        </aside>
+        <main className="flex-1 p-6 overflow-y-auto">
+          {children}
+        </main>
+      </div>
+    </div>
   );
 }
