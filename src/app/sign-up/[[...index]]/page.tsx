99e7a12d7ae196a8f8afc21e9788fc524b0391a1
medicalexcom <medicalex.com@gmail.com>
2025-11-22 02:20:39 -0600

    Add afterSignUpUrl to SignUp component

--- src/app/sign-up/[[...index]]/page.tsx
+++ src/app/sign-up/[[...index]]/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { SignUp } from '@clerk/nextjs';
+import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-50 px-4 py-12">
       <div className="w-full max-w-md rounded-2xl bg-white p-8 shadow">
-        <SignUp routing="path" path="/sign-up" signInUrl="/sign-in" />
+        <SignUp routing="path" path="/sign-up" signInUrl="/sign-in" afterSignUpUrl="/trial-setup" />
       </div>
     </div>
   );
