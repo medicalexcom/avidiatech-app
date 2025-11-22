f70c4a56df6992ee86b1f6f772dba300ce6fa10a
medicalexcom <medicalex.com@gmail.com>
2025-11-22 01:39:30 -0600

    Delete src directory

--- src/app/providers.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import type { ReactNode } from 'react';
-
-export default function Providers({ children }: { children: ReactNode }) {
-  // if you add ThemeProvider, QueryClientProvider, etc, they go here
-  return <>{children}</>;
-}
