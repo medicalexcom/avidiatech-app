0e76c8481ee2aa93039721a5723bc6c4e06e7ea1
medicalexcom <medicalex.com@gmail.com>
2025-11-26 22:45:49 -0600

    Delete tools/render-engine/server-boot-snippet.js

--- tools/render-engine/server-boot-snippet.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-// Add request logger and the /describe handler immediately
-require('./request-logger')(app);
-require('./describe-handler')(app);
-
-// ...your existing middleware and routes continue here...
