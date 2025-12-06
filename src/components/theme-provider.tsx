457d0c07262cb737324b8d25cf17b425de3770f8
medicalexcom <medicalex.com@gmail.com>
2025-12-06 03:20:32 -0600

    Add ThemeProvider component for theme management

---
+++ src/components/theme-provider.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import type { ThemeProviderProps } from "next-themes/dist/types";
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return (
+    <NextThemesProvider
+      attribute="class"          // adds "class" to <html>
+      defaultTheme="light"       // 👈 light by default
+      enableSystem={false}       // ignore OS theme for now (can set true later)
+      storageKey="avidia-theme"  // key in localStorage
+      disableTransitionOnChange
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  );
+}
