c841371d596c45c1a7f0b07dc812d0a7e20b2572
medicalexcom <medicalex.com@gmail.com>
2025-12-15 10:09:38 -0600

    Delete .next/server/app/api/v1/pipeline/run/[id]/module/[index]/[index] directory

--- .next/server/app/api/v1/pipeline/run/[id]/module/[index]/[index]/logs/route_client-reference-manifest.js
+++
@@ -1,2 +0,0 @@
-globalThis.__RSC_MANIFEST = globalThis.__RSC_MANIFEST || {};
-globalThis.__RSC_MANIFEST["/api/v1/pipeline/run/[id]/module/[index]/[index]/logs/route"] = {"moduleLoading":{"prefix":"","crossOrigin":null},"clientModules":{},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"rscModuleMapping":{},"edgeRscModuleMapping":{},"entryCSSFiles":{},"entryJSFiles":{}}
