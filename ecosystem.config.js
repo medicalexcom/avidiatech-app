183318c7257341f0f1d2b799828569b34c2f7ac5
medicalexcom <medicalex.com@gmail.com>
2025-12-30 21:14:22 -0600

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,8 @@ module.exports = {
       script: "./dist/workers/bulkMasterWorker.js",
       instances: 1,
       exec_mode: "fork",
+      autorestart: true,
+      watch: false,
       env: {
         NODE_ENV: "production"
       },
@@ -17,6 +19,8 @@ module.exports = {
       script: "./dist/workers/bulkItemWorker.js",
       instances: 2,
       exec_mode: "fork",
+      autorestart: true,
+      watch: false,
       env: {
         NODE_ENV: "production",
         BULK_ITEM_CONCURRENCY: 8
