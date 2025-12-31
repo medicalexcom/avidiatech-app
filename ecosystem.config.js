9fb10b7494b1abba45340056cc549a2afdf341c4
medicalexcom <medicalex.com@gmail.com>
2025-12-31 17:14:44 -0600

    Add environment variables to ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,12 @@ module.exports = {
       autorestart: true,
       watch: false,
       env: {
-        NODE_ENV: "production"
+        NODE_ENV: "production",
+        INTERNAL_API_BASE: "https://app.avidiatech.com",
+        SERVICE_API_KEY: "PUT_REAL_SERVICE_API_KEY_HERE",
+        SUPABASE_URL: "https://dpomficpjunfzkyknzrq.supabase.co",
+        SUPABASE_SERVICE_ROLE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwb21maWNwanVuZnpreWtuenJxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MzMyOTk4NCwiZXhwIjoyMDc4OTA1OTg0fQ.6zJT08EvDmrigdD-Gmcx931cf3WOKYv0ob0IZzPATKI",
+        REDIS_URL: "rediss://default:AVnvAAIncDFmOWY4ZmY2N2ZhZWI0ZDExYjgwYmVmMDU4ZmRmMjg2YXAxMjMwMjM@smart-dassie-23023.upstash.io:6379"
       }
     },
     {
@@ -20,8 +25,13 @@ module.exports = {
       watch: false,
       env: {
         NODE_ENV: "production",
-        BULK_ITEM_CONCURRENCY: 8
+        INTERNAL_API_BASE: "https://app.avidiatech.com",
+        SERVICE_API_KEY: "PUT_REAL_SERVICE_API_KEY_HERE",
+        SUPABASE_URL: "https://dpomficpjunfzkyknzrq.supabase.co",
+        SUPABASE_SERVICE_ROLE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwb21maWNwanVuZnpreWtuenJxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MzMyOTk4NCwiZXhwIjoyMDc4OTA1OTg0fQ.6zJT08EvDmrigdD-Gmcx931cf3WOKYv0ob0IZzPATKI",
+        REDIS_URL: "rediss://default:AVnvAAIncDFmOWY4ZmY2N2ZhZWI0ZDExYjgwYmVmMDU4ZmRmMjg2YXAxMjMwMjM@smart-dassie-23023.upstash.io:6379",
+        BULK_ITEM_CONCURRENCY: "8"
       }
     }
   ]
-};
+};    
