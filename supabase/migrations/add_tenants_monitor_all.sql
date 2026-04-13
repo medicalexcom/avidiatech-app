416e6134c61b5010a402899beea24e2c950b1e9d
medicalexcom <medicalex.com@gmail.com>
2026-04-12 23:34:54 -0500

    dd

--- supabase/migrations/add_tenants_monitor_all.sql
+++
@@ -1,9 +0,0 @@
--- Add tenant-level setting: monitor_all (default true)
--- Idempotent.
-
-BEGIN;
-
-ALTER TABLE IF EXISTS public.tenants
-  ADD COLUMN IF NOT EXISTS monitor_all boolean NOT NULL DEFAULT true;
-
-COMMIT;
